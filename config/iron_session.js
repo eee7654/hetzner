1d3088eeed7dc42bf41929da065b26afdadd0770
Ehsan Mashayekh <e.mashayekh@live.com>
2023-09-16 00:01:52 +0330

    aaa

--- config/iron_session.js
+++ config/iron_session.js
@@ -4,7 +4,7 @@ const sessionOptions = {
   password: process.env.COOKIE_PASS,
   cookieName: process.env.COOKIE_NAME,
   cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
     /*maxAge:60*1000*/
   }/*,
   ttl:60*/
